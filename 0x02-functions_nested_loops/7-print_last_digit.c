bec31dc00f1d741b9c913770bb8a6802ad71d730
Efeoseaje <efeoseaje@yahoo.com>
2022-09-14 02:37:09 +0100

    last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,17 +9,12 @@
 
 int print_last_digit(int n)
 {
-	int last;
+	int last = n % 10;
 
-	if (n > 0)
-	{
-		last = n % 10;
-		_putchar(last + '0');
-	}
-	else
-	{
-		last = -1 * n % 10;
-		_putchar(last + '0');
-	}
-	return (last % 10);
+	if (n < 0)
+		last = last * -1;
+
+	_putchar(last + '0');
+
+	return (last);
 }
