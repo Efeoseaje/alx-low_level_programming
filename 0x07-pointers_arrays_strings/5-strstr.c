46e1bca0a6de2adea5b6f187ea3056cfe60d0243
Efeoseaje <efeoseaje@yahoo.com>
2022-09-27 01:46:01 +0100

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,7 +6,8 @@
  * @haystack: This is the input string
  * @needle: This is a find to locate in the string
  *
- * Return: a pointer to the beginning of the located substring, or NULL if no match
+ * Return: a pointer to the beginning of the located substring, or NULL if the
+ * substring is not found
  */
 
 char *_strstr(char *haystack, char *needle)
