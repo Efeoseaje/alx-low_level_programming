6a3a69b6bb31e6295cba8f34116c60c8f5300ca3
Efeoseaje <efeoseaje@yahoo.com>
2022-11-05 22:47:15 +0100

    prints string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,13 @@
 
 void print_rev(char *s)
 {
-	int i = 0, j, len = 0;
+	int i = 0;
 
 	while (s[i] != '\0')
-		len++;
-	for (j = len - 1; s[j] != '\0'; j--)
+		i++;
+	for (i = i - 1; s[i] != '\0'; i--)
 	{
-		_putchar(s[j]);
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
