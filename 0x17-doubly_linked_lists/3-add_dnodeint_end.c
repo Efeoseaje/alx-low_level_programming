05a2096d92ccfe0a71ac6cae25061e3aacf485de
Efeoseaje <efeoseaje@yahoo.com>
2023-03-17 23:08:06 +0100

    add to the end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *newnode = malloc(sizeof(dlistint_t));
 	dlistint_t *temp = *head;
-	
+
 	if (!newnode)
 		return (NULL);
 
